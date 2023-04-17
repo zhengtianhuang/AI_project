53763fec517951679dcdfa3e1cbf8aa119bd434a
mikeyhangover <128007595+mikeyhangover@users.noreply.github.com>
2023-04-17 14:58:51 +0800

    richmenu更新

--- side_projects/richmenu/autoUpload.py
+++ side_projects/richmenu/autoUpload.py
@@ -16,34 +16,62 @@ data = {
         "height": 1686
     },
     "selected": "true",
-    "name": "圖文選單 2",
+    "name": "圖文選單 1",
     "chatBarText": "查看更多資訊",
     "areas": [
         {
             "bounds": {
-                "x": 1778,
-                "y": 165,
-                "width": 153,
-                "height": 144
+                "x": 172,
+                "y": 625,
+                "width": 618,
+                "height": 409
             },
             "action": {
-                "type": "uri",
-                "uri": "https://ithelp.ithome.com.tw/m/articles/10271673"
+                "type": "postback",
+                "text": "功能介紹",
+                "data": "功能介紹"
             }
         },
         {
             "bounds": {
-                "x": 1960,
-                "y": 957,
-                "width": 367,
-                "height": 371
+                "x": 1717,
+                "y": 621,
+                "width": 614,
+                "height": 435
             },
             "action": {
-                "type": "message",
-                "text": "\b\b你好"
+                "type": "postback",
+                "text": "寵物資料",
+                "data": "寵物資料"
+            }
+        },
+        {
+            "bounds": {
+                "x": 165,
+                "y": 1070,
+                "width": 625,
+                "height": 521
+            },
+            "action": {
+                "type": "postback",
+                "text": "寵物情緒分析",
+                "data": "寵物情緒分析"
+            }
+        },
+        {
+            "bounds": {
+                "x": 1724,
+                "y": 1074,
+                "width": 596,
+                "height": 510
+            },
+            "action": {
+                "type": "postback",
+                "text": "新增資料",
+                "data": "新增資料"
             }
         }
-    ],
+    ]
 }
 response = requests.post(url, headers=headers, json=data)
 richmenu_id = response.json()["richMenuId"]
