c4f33ee95932408091a6ce46414c243cfaf3a7f1
Millie-8911 <128894092+Millie-8911@users.noreply.github.com>
2023-04-20 16:30:28 +0800

    更新MySQL, 新增server.py CRUD methods

--- MySQL/linebot_project.sql
+++ MySQL/linebot_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： localhost
--- 產生時間： 2023 年 04 月 17 日 07:04
+-- 產生時間： 2023 年 04 月 20 日 10:21
 -- 伺服器版本： 10.4.27-MariaDB
 -- PHP 版本： 8.2.0
 
@@ -24,19 +24,84 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `emotion`
+--
+
+CREATE TABLE `emotion` (
+  `emotion_id` int(11) NOT NULL,
+  `emotion` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `emotion`
+--
+
+INSERT INTO `emotion` (`emotion_id`, `emotion`) VALUES
+(1, '開心'),
+(2, '生氣'),
+(3, '難過'),
+(4, '放鬆');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `emotion_record`
+--
+
+CREATE TABLE `emotion_record` (
+  `pet_id` varchar(50) NOT NULL,
+  `emotion_photo` varchar(100) NOT NULL,
+  `emotion` varchar(50) NOT NULL,
+  `updated_time` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `emotion_record`
+--
+
+INSERT INTO `emotion_record` (`pet_id`, `emotion_photo`, `emotion`, `updated_time`) VALUES
+('1', 'happ.jpg', '開心', '2023-04-20 10:36:15'),
+('1', 'emoaional.jpg', '放鬆', '2023-04-20 15:30:47'),
+('1', 'relaxed.jpg', '放鬆', '2023-04-20 15:31:00'),
+('1', 'relaxed.jpg', '放鬆', '2023-04-20 15:58:00');
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `pet`
 --
 
 CREATE TABLE `pet` (
   `user_id` varchar(50) NOT NULL,
-  `pet_id` varchar(50) NOT NULL,
   `pet_name` varchar(50) NOT NULL,
-  `pet_photo` blob NOT NULL,
-  `recent_emotion` varchar(300) NOT NULL,
+  `pet_photo` varchar(100) DEFAULT NULL,
   `pet_breed` varchar(50) NOT NULL,
-  `created_time` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_time` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `pet_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `pet`
+--
+
+INSERT INTO `pet` (`user_id`, `pet_name`, `pet_photo`, `pet_breed`, `pet_id`) VALUES
+('U751dd717d052680824fd250ddb7a7a55', 'Golden', 'cde.png', '黃金獵犬', 1),
+('oljimmm', 'jimmy', '5678.png', '吉娃娃', 4),
+('U751dd717d052680824fd250ddb7a7a55', 'KOKO', '1234.png', '邊牧', 5),
+('KIOLLLL', 'Oreo2', 'okok.png', '大麥町', 8),
+('KIOLLLL', 'KIKI', '5678.png', '吉娃娃', 9),
+('KIOLLLL', 'coco', '5678.png', '吉娃娃', 10),
+('KIOLLLL', 'jj', '5678.png', '吉娃娃', 11),
+('KIOLLLL', 'aa', '5678.png', '吉娃娃', 12),
+('KIOLLLL', 'cc', '5678.png', '吉娃娃', 14),
+('KIOLLLL', 'dd', '5678.png', '土狗', 15),
+('KIOLLLL', 'ff', '5678.png', '黑狗', 16),
+('KIOLLLL', 'ee', '5678.png', '黃狗', 17),
+('KIOLLLL', 'jiok', '5678.png', '黃狗', 18),
+('KIOLLLL', 'ji', '5678.png', '黃狗', 19),
+('KIOLLLL', 'j', '5678.png', '黃狗', 20),
+('KIOLLLL', 'j1234', '5678.png', '黃狗', 21),
+('KIOLLLL', 'j2345', '5678.png', '胖狗', 24),
+('KIOLLLL', 'lol', '5678.png', '大胖狗', 51);
 
 -- --------------------------------------------------------
 
@@ -45,29 +110,69 @@ CREATE TABLE `pet` (
 --
 
 CREATE TABLE `user` (
-  `user_id` varchar(100) NOT NULL,
-  `created_time` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_time` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `user_id` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- 傾印資料表的資料 `user`
 --
 
-INSERT INTO `user` (`user_id`, `created_time`, `updated_time`) VALUES
-('12345', '2023-04-16 17:50:49', '2023-04-16 17:50:49'),
-('abc', '2023-04-16 17:46:53', '2023-04-16 17:46:53'),
-('U751dd717d052680824fd250ddb7a7a55', '2023-04-17 11:31:40', '2023-04-17 11:31:40');
+INSERT INTO `user` (`user_id`) VALUES
+('0000'),
+('1'),
+('11'),
+('12345'),
+('123456'),
+('188'),
+('4040'),
+('5'),
+('6'),
+('aaa'),
+('aaaa'),
+('abc'),
+('ijjm1122'),
+('ohya'),
+('popo'),
+('U751dd717d052680824fd250ddb7a7a55'),
+('U8f511f37ee2064e1868f50fb8082dd9f');
 
 --
 -- 已傾印資料表的索引
 --
 
 --
+-- 資料表索引 `emotion`
+--
+ALTER TABLE `emotion`
+  ADD PRIMARY KEY (`emotion_id`);
+
+--
+-- 資料表索引 `pet`
+--
+ALTER TABLE `pet`
+  ADD PRIMARY KEY (`pet_id`);
+
+--
 -- 資料表索引 `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `emotion`
+--
+ALTER TABLE `emotion`
+  MODIFY `emotion_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `pet`
+--
+ALTER TABLE `pet`
+  MODIFY `pet_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- database.py
+++ database.py
@@ -13,70 +13,216 @@ def connect_database():
     )
     return db
 
+# if cursor.fetchall() find nothing return () -> empty tuple
+# cursor.fetchall() find data return ([])  -> list in tuple
 
-def now_datetime():
-    now = datetime.now()
-    formatted_time = now.strftime('%Y-%m-%d %H:%M:%S')
-    return formatted_time
+
+def execute_sql(sql, args=None):
+    # used for SELECT method
+    db = connect_database()
+    cursor = db.cursor()
+    try:
+        with cursor:
+            cursor.execute(sql, args)
+            db.commit()
+            # return match data
+            return cursor.fetchall()
+    except Exception as e:
+        db.rollback()
+    finally:
+        cursor.close()  # turn off cursor
+        db.close()
+
+
+def execute_sql_2(sql, args=None):
+    # used for INSERT, UPDATE, DELETE method
+    db = connect_database()
+    cursor = db.cursor()
+    try:
+        with cursor:
+            cursor.execute(sql, args)
+            db.commit()
+            # return number of changed data
+            return cursor.rowcount
+    except Exception as e:
+        db.rollback()
+        return 0
+    finally:
+        cursor.close()
+        db.close()
 
 
 def user_id_exists(user_id):
-    db = connect_database()
-    cursor = db.cursor()  # 連線後的游標
+    # 每次使用者傳訊息皆須判斷
+    # Check if user_id exists in the database
+    sql = 'SELECT * FROM `user` WHERE `user_id` = %s'
     try:
-        with cursor:
-            # Check if user_id exists in the database
-            sql = 'SELECT * FROM `user` WHERE `user_id` = %s'
-            cursor.execute(sql, (user_id,))
-            result = cursor.fetchall()
-            # if cursor.fetchall() find nothing return () -> empty tuple
-            if result and isinstance(result, tuple) and len(result) > 0:
-                print("資料已存在DB")
-            elif isinstance(result, tuple) and len(result) == 0:
-                # If user_id does not exist, insert a new row into the database
-                sql = 'INSERT INTO `user` (`user_id`) VALUES (%s)'
-                cursor.execute(sql, (user_id,))
-                db.commit()
-                # test whether the userid is insert into database successfully
-                cursor.execute(
-                    "SHOW KEYS FROM `user` WHERE Key_name = 'PRIMARY'")
-                result_set = cursor.fetchall()
-                if result_set:
-                    # insert success
-                    print("資料加入DB成功")
-                else:
-                    # insert failed
-                    print("資料加入DB失敗")
+        result = execute_sql(sql, (user_id,))
     except Exception as e:
-        db.rollback()
-        print(f'SQL 執行失敗: {e}')
-    finally:
-        cursor.close()  # turn off cursor
-        db.close()
+        print(f"SQL 錯誤: {str(e)}")
+
+    if result:
+        print("資料已存在DB")
+    else:
+        sql = 'INSERT INTO `user` (`user_id`) VALUES (%s)'
+        try:
+            result_insert = execute_sql_2(sql, (user_id,))
+        except Exception as e:
+            print(f"SQL 錯誤: {str(e)}")
+
+        if result_insert > 0:
+            print("加入成功")
+        else:
+            print("加入失敗")
 
 
 def append_pet(user_id, pet_name, pet_photo, pet_breed):
-    db = connect_database()
-    cursor = db.cursor()  # 連線後的游標
+    # check if the pet already exists
+    sql = 'SELECT `user_id`, `pet_name` FROM `pet` WHERE `user_id` = %s AND `pet_name` = %s'
     try:
-        with cursor:
-            # check whether data exists in database
-            sql = 'SELECT `user_id`, `pet_name`,`pet_breed` FROM `pet` WHERE `user_id` = %s AND `pet_name` = %s AND `pet_breed` = %s'
-            cursor.execute(sql, (user_id, pet_name, pet_breed))
-            result = cursor.fetchall()
-            print(result)
-            # if cursor.fetchall() find nothing return () -> empty tuple
-            if result and isinstance(result, tuple) and len(result) > 0:
-                print("資料已存在DB")
-            elif isinstance(result, tuple) and len(result) == 0:
-                # If user_id does not exist, insert a new row into the database
-                sql = 'INSERT INTO `pet`(`user_id`, `pet_name`, `pet_photo`, `pet_breed`) VALUES (%s , %s , %s , %s)'
-                cursor.execute(sql, (user_id, pet_name, pet_photo, pet_breed))
-                db.commit()
-                print("資料加入DB成功")
+        result = execute_sql(sql, (user_id, pet_name))
     except Exception as e:
-        db.rollback()
-        print(f'SQL 執行失敗: {e}')
-    finally:
-        cursor.close()  # turn off cursor
-        db.close()
+        print(f"SQL 錯誤: {str(e)}")
+        return False
+
+    if result:
+        print("寵物名稱重複")
+        return ("寵物名稱重複")
+    else:
+        sql = 'INSERT INTO  `pet`(`user_id`, `pet_name`, `pet_photo`, `pet_breed`) VALUES (%s , %s , %s , %s)'
+        try:
+            result_insert = execute_sql_2(
+                sql, (user_id, pet_name, pet_photo, pet_breed))
+        except Exception as e:
+            print(f"SQL 錯誤: {str(e)}")
+            return False
+
+        if result_insert > 0:
+            print("新增成功")
+            return ("新增成功")
+        else:
+            print("新增失敗")
+            return ("新增失敗")
+
+
+def revise_pet(user_id, pet_name, pet_photo, pet_breed, origin_name):
+    sql = 'UPDATE `pet` SET `pet_name`= %s ,`pet_photo`= %s ,`pet_breed`= %s WHERE (`user_id` = %s) AND (`pet_name` = %s)'
+    # for testing
+    # get originl pet_name from json
+    try:
+        result_update = execute_sql_2(
+            sql, (pet_name, pet_photo, pet_breed, user_id, origin_name))
+    except Exception as e:
+        print(f"SQL 錯誤: {str(e)}")
+        return False
+    if result_update > 0:
+        print("更新成功")
+        return ("更新成功")
+    else:
+        print("更新失敗")
+        return ("更新失敗")
+
+
+def delete_pet(user_id, pet_name):
+    # delete data in `emotion_record` table
+    sql2 = 'DELETE FROM `emotion_record` WHERE `pet_id` IN (SELECT `pet_id` FROM `pet` WHERE `user_id` = %s AND pet_name = %s)'
+    try:
+        execute_sql_2(sql2, (user_id, pet_name))
+    except Exception as e:
+        print(f"SQL 錯誤: {str(e)}")
+        return False
+    # delete data in `pet` table
+    sql = 'DELETE FROM `pet` WHERE `user_id` = %s AND `pet_name` = %s'
+    try:
+        result_delete_pet = execute_sql_2(sql, (user_id, pet_name))
+    except Exception as e:
+        print(f"SQL 錯誤: {str(e)}")
+        return False
+
+    if result_delete_pet > 0:
+        print("刪除成功")
+        return ("刪除成功")
+    else:
+        print("刪除失敗")
+        return ("刪除失敗")
+
+
+def find_pet_id(user_id, pet_name):
+    sql = 'SELECT `user_id`, `pet_name` , `pet_id` FROM `pet` WHERE `user_id` = %s AND `pet_name` = %s'
+    try:
+        result = execute_sql(sql, (user_id, pet_name))
+    except Exception as e:
+        print(f"SQL 錯誤: {str(e)}")
+    if result:
+        # get pet_id in return tuple
+        pet_id = result[0][2]
+        print(pet_id)
+        return int(pet_id)
+    else:
+        return None
+
+
+def append_emotion(emotion_photo, emotion, user_id, pet_name):
+    sql = 'INSERT INTO  `emotion_record` (`pet_id`, `emotion_photo`, `emotion`) VALUES (%s , %s , %s)'
+    try:
+        result = execute_sql_2(
+            sql, (find_pet_id(user_id, pet_name), emotion_photo, emotion))
+    except Exception as e:
+        print(f"SQL 錯誤: {str(e)}")
+        return False
+
+    if result > 0:
+        print("新增分析結果成功")
+        return("新增分析結果成功")
+    else:
+        print("新增分析結果失敗")
+        return("新增分析結果失敗")
+
+
+def show_all_pet(user_id):
+    sql = 'SELECT `pet_id` FROM `pet` WHERE `user_id` = %s'
+    try:
+        # result return a tuple
+        result_pet = execute_sql(sql, (user_id))
+    except Exception as e:
+        print(f"SQL 錯誤: {str(e)}")
+        return False
+    if result_pet:
+        sql = 'SELECT * FROM `emotion_record` WHERE `pet_id` IN (SELECT `pet_id` FROM `pet` WHERE `user_id` = %s)'
+        try:
+            # result return a tuple
+            result_emotion = execute_sql(sql, (user_id))
+            pet_list = []
+        except Exception as e:
+            print(f"SQL 錯誤: {str(e)}")
+            return False
+        if result_emotion:
+            # switch tuple to dict
+            for row in result_emotion:
+                pet_dict = {
+                    'pet_id': row[0],
+                    'emotion_photo': row[1],
+                    'emotion': row[2],
+                    'updated_time': row[3]
+                }
+            pet_list.append(pet_dict)
+            print(pet_list)
+            return("顯示寵物資料")
+        else:
+            print("尚無寵物情情緒分析結果！")
+            return("尚無寵物情情緒分析結果！")
+    else:
+        print("尚無添加寵物")
+        return("尚無添加寵物")
+
+
+# user_id_exists('6')
+# user_id_exists('188')
+# append_pet('KIOLLLL', 'lol', '5678.png', '大胖狗')
+# revise_pet('KIOLLLL', 'Oreo2', 'okok.png', '大麥町', 'Oreo')
+# delete_pet('KIOLLLL', 'bb')
+# find_pet_id('U751dd717d052680824fd250ddb7a7a55', 'Golden')
+# append_emotion('relaxed.jpg', '放鬆',
+#                'U751dd717d052680824fd250ddb7a7a55', 'Golden')
+# show_all_pet('U751dd717d052680824fd250ddb7a7a55')
+# show_all_pet('KIOLLLL')
