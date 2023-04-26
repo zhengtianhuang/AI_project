96d98e0c467b5331e40f6474ec74362a7266c09c
zhengtianhuang <zhengtian0629@gmail.com>
2023-04-26 11:12:41 +0800

    訓練模型

--- tasks/train_model/predict.py
+++ tasks/train_model/predict.py
@@ -1,27 +1,35 @@
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
-import tensorflow as tf
-from pathlib import Path
 import cv2
+from keras.models import load_model
+from pathlib import Path
 filePath = Path(__file__).resolve().parent
 
-# 載入模型
-model = tf.keras.models.load_model(filePath/'0425.h5', compile=False)
 
-# 設定圖片尺寸及通道數
-img_size = (48, 48)
-img_channels = 1
+def showImg(img):
+    cv2.imshow("img", img)
+    cv2.waitKey()
 
-# 讀取圖片
-img_path = filePath/'img/2AoiPsaOr4YiEGxiOAhfMQpZ3K77nc168.jpeg'
-img = cv2.imread(str(img_path), cv2.IMREAD_GRAYSCALE)
-img = cv2.resize(img, img_size)
-img = np.reshape(img, (1, img_size[0], img_size[1], img_channels))
+
+# 加載已經訓練好的模型
+model = load_model(filePath/'0425.h5')
+
+# 讀取新的狗圖像
+img = cv2.imread(str(filePath/'img/0fmy0dTY2aV0kGQ8UEDgWDrbGyxnac904.jpeg'))
+# showImg(img)
+
+img = cv2.resize(img, (150, 150))
+# showImg(img)
+
+# 預處理圖像
 img = img.astype('float32') / 255.0
+# showImg(img)
+
+img = np.expand_dims(img, axis=0)
+# print(model.summary())
+# img = np.reshape(img, (-1, 4 * 4 * 512))
+# showImg(img)
+
 
 # 進行預測
-emo = ['angry', 'happy', 'relaxed', 'sad']
 predictions = model.predict(img)
-label = np.argmax(predictions)
 print(predictions)
-print(emo[label])
---
+++ tasks/train_model/train.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from pathlib import Path
+filePath = Path(__file__).resolve().parent
+root_dir = filePath/'content/Dog Emotion'
+base_model = tf.keras.applications.InceptionV3(input_shape=(224, 224, 3),
+                                               include_top=False,
+                                               weights="imagenet")
+# https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/image/ImageDataGenerator
+
+img_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+    # rotation_range=90,
+    brightness_range=(0.5, 1),
+    # shear_range=0.2,
+    # zoom_range=0.2,
+    channel_shift_range=0.2,
+    horizontal_flip=False,
+    vertical_flip=False,
+    rescale=1./255,
+    validation_split=0.3)
+
+img_generator_flow_train = img_generator.flow_from_directory(
+    directory=root_dir,
+    target_size=(224, 224),
+    batch_size=32,
+    shuffle=True,
+    subset="training")
+
+img_generator_flow_valid = img_generator.flow_from_directory(
+    directory=root_dir,
+    target_size=(224, 224),
+    batch_size=32,
+    shuffle=True,
+    subset="validation")
+imgs, labels = next(iter(img_generator_flow_train))
+base_model.trainable = False
+model = tf.keras.Sequential([
+    base_model,
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(4, activation="softmax")  # 4 classes
+])
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+              loss=tf.keras.losses.CategoricalCrossentropy(),
+              metrics=[tf.keras.metrics.CategoricalAccuracy()])
+model.fit(img_generator_flow_train,
+          validation_data=img_generator_flow_valid,
+          steps_per_epoch=10, epochs=100)
