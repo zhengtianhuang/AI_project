014e7260d8516c837051be519c8d142eaecf8aa0
zhengtianhuang <zhengtian0629@gmail.com>
2023-04-26 15:45:49 +0800

    預測程式完成

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import requests
+from google.cloud import vision_v1
+from google.cloud.vision_v1 import types
 from database import append_pet, user_id_exists, update_pet, delete_pet
 import time
 from pathlib import Path
@@ -112,9 +114,9 @@ class PetCreator:
         elif self.steps[user_id] == 1:
             self.steps[user_id] = 2
         elif self.steps[user_id] == 2:
-            self.steps[user_id] = 3
-        elif self.steps[user_id] == 3:
-            return 4
+            return 3
+        # elif self.steps[user_id] == 3:
+        #     return 4
         return self.steps[user_id]
 
     def _save_img(self, id, content):
@@ -130,11 +132,11 @@ class PetCreator:
     def create_pet(self, user_id, step, data):
         if step == 2:
             self.name[user_id] = data
-            return f"你的寵物名字是{data},請輸入品種"
+            return f"你的寵物名字是{data},請傳一張照片"
+        # elif step == 3:
+        #     self.breed[user_id] = data
+        #     return f"品種為{data},請傳一張照片！"
         elif step == 3:
-            self.breed[user_id] = data
-            return f"品種為{data},請傳一張照片！"
-        elif step == 4:
             # user_id_exists(user_id)
             # append_pet(user_id, self.name[user_id],
             #            data, self.breed[user_id])
@@ -146,14 +148,99 @@ class PetCreator:
         self.steps[user_id] = 0
         file_name = self._save_img(img_id, content)
         # print(f"{user_id},{self.name[user_id]},{self.breed[user_id]}")
+        imgUrl = os.path.join(
+            os.getenv('WEBHOOK_URL'), 'static/img', file_name)
+        self.breed[user_id] = search_image(imgUrl)
         user_id_exists(user_id)
         append_pet(user_id, self.name[user_id],
                    file_name, self.breed[user_id])
         return (f"名字：{self.name[user_id]},品種：{self.breed[user_id]}")
-    def update_pet_img(self,user_id,img_id,content):
+
+    def update_pet_img(self, user_id, img_id, content):
         if self.updateCol[user_id] != 1:
             return 0
         self.updateCol[user_id] = 0
         file_name = self._save_img(img_id, content)
         update_pet("pet_photo", file_name, user_id, self.updateNum[user_id])
-        
+
+
+def search_image(url):
+    params = {
+        "engine": "google_lens",
+        "url": url,
+        "api_key": "7bec1da4ca2d934e97d542f96113cad7e2b7052e13098b7741416c75e8221073",
+        "hl": "zh-tw"
+    }
+
+    url = "https://serpapi.com/search.json?engine=google_lens&url=" + url
+
+    response = requests.get(url, data=params)
+    response.encoding = "utf-8"
+
+    content = response.json()
+    print(url)
+    try:
+        breed = (content['knowledge_graph'][0])['title']
+    except:
+        breed = "未知"
+    return breed
+
+
+url = "https://fdaa-210-242-167-237.ngrok-free.app/static/img/18042841890258-1682478441.jpg"
+
+
+def search_image(url):
+
+    params = {
+        "engine": "google_lens",
+        "url": url,
+        "api_key": "7bec1da4ca2d934e97d542f96113cad7e2b7052e13098b7741416c75e8221073",
+        "hl": "zh-tw"
+    }
+
+    request_url = "https://serpapi.com/search.json?engine=google_lens&url=" + url
+
+    response = requests.get(request_url, data=params)
+    response.encoding = "utf-8"
+
+    content = response.json()
+    print(content)
+    try:
+        breed = (content['knowledge_graph'][0])['title']
+    except:
+        breed = "未知"
+    return breed
+
+
+def image_content(image_url):
+    # 設定您的Google Cloud認證環境變數
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./zhengtian-488e92363e42.json"
+
+    # 建立Vision API客戶端
+    client = vision_v1.ImageAnnotatorClient()
+
+    # 得到圖片名稱
+    # image_name = url.split('/')
+
+    # 讀取本地圖片文件
+    with open(image_url, 'rb') as image_file:
+        content = image_file.read()
+
+    # 建立Image類型的protobuf對象
+    image = types.Image(content=content)
+
+    # 使用Vision API進行圖像搜尋
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    # 過濾出狗的品種
+    for label in labels:
+        if label.description == "Dog breed":
+            return 1
+            #     return search_image(url)
+            #     break
+    return 0
+
+
+# print(image_content(
+#     "./richmenu.png"))
