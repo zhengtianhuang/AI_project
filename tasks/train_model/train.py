12884ea8fd5c129f1468b43f7df2c603b131f47f
mikeyhangover <128007595+mikeyhangover@users.noreply.github.com>
2023-05-04 16:46:23 +0800

    模型測試參數更改

--- tasks/train_model/train.py
+++ tasks/train_model/train.py
@@ -4,13 +4,14 @@ import os
 import matplotlib.pyplot as plt
 from pathlib import Path
 import json
+from tensorflow.keras.models import load_model
 print(tf.version)
 filePath = Path(__file__).resolve().parent
 img_dir = filePath/'content/Dog Emotion'
-base_model = tf.keras.applications.InceptionV3(input_shape=(224, 224, 3),
-                                               include_top=False,
-                                               weights="imagenet")
-# https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/image/ImageDataGenerator
+# base_model = tf.keras.applications.InceptionV3(input_shape=(224, 224, 3),
+#                                                include_top=False,
+#                                                weights="imagenet")
+# # https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/image/ImageDataGenerator
 
 img_generator = tf.keras.preprocessing.image.ImageDataGenerator(
     # rotation_range=90,
@@ -36,21 +37,32 @@ img_generator_flow_valid = img_generator.flow_from_directory(
     batch_size=32,
     shuffle=True,
     subset="validation")
-imgs, labels = next(iter(img_generator_flow_train))
-base_model.trainable = False
-model = tf.keras.Sequential([
-    base_model,
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(4, activation="softmax")  # 4 classes
-])
+# imgs, labels = next(iter(img_generator_flow_train))
+# base_model.trainable = False
+# model = tf.keras.Sequential([
+#     base_model,
+#     tf.keras.layers.MaxPooling2D(),
+#     tf.keras.layers.Flatten(),
+#     tf.keras.layers.Dropout(0.5),
+#     tf.keras.layers.Dense(256, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
+#     tf.keras.layers.Dropout(0.5),
+#     tf.keras.layers.Dense(4, activation="softmax")
+# ])  # 4 classes
+model = load_model(
+    filePath/'model/0504_10epeh500pochs.h5')  # 加載已經訓練好的模型
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
               loss=tf.keras.losses.CategoricalCrossentropy(),
               metrics=[tf.keras.metrics.CategoricalAccuracy()])
 
 history = model.fit(img_generator_flow_train,
                     validation_data=img_generator_flow_valid,
+<<<<<<< Updated upstream
                     steps_per_epoch=10, epochs=200)
 model.save(filePath/'model/0504.h5')
 with open(filePath/'model/0504.json', "w") as json_file:
+=======
+                    steps_per_epoch=10, epochs=300)
+model.save(filePath/'model/0504_10epeh800pochs.h5')
+with open(filePath/'model/0504_10epeh800pochs.json', "w") as json_file:
+>>>>>>> Stashed changes
     json.dump(history.history, json_file)
