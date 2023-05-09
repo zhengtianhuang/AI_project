77b68892d4c31abd0bb63fc6da2dfa0066ec0fe5
zhengtianhuang <zhengtian0629@gmail.com>
2023-05-09 10:03:13 +0800

    畫出模型訓練過程

--- Tasks/train_model/test_accurancy.py
+++ Tasks/train_model/test_accurancy.py
@@ -3,13 +3,14 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 
 file_path = Path(__file__).resolve().parent
-with open(file_path/'model/0504_10epeh800pochs.json', 'r') as f:
+with open(file_path/'model/0504.json', 'r') as f:
     history_dict = json.load(f)
 
 fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(8, 8))
 
-ax1.plot(history_dict["categorical_accuracy"], c="r", label="train_accuracy")
-ax1.plot(history_dict["val_categorical_accuracy"], c="b", label="test_accuracy")
+ax1.plot(history_dict["acc"], c="r", label="train_accuracy")
+ax1.plot(history_dict["val_acc"],
+         c="b", label="test_accuracy")
 ax1.legend(loc="upper left")
 
 ax2.plot(history_dict["loss"], c="g", label="loss")
