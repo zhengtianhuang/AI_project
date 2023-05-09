b6d7acd032bcb8122722c46a0c46f7935ca49e4b
mikeyhangover <128007595+mikeyhangover@users.noreply.github.com>
2023-05-09 15:10:05 +0800

    新增test loss曲線

--- Tasks/train_model/test_accurancy.py
+++ Tasks/train_model/test_accurancy.py
@@ -1,3 +1,25 @@
+# 未顯示test loss曲線
+
+# import json
+# from pathlib import Path
+# import matplotlib.pyplot as plt
+
+# file_path = Path(__file__).resolve().parent
+# with open(file_path/'model/0504.json', 'r') as f:
+#     history_dict = json.load(f)
+
+# fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(8, 8))
+
+# ax1.plot(history_dict["acc"], c="r", label="train_accuracy")
+# ax1.plot(history_dict["val_acc"],
+#          c="b", label="test_accuracy")
+# ax1.legend(loc="upper left")
+
+# ax2.plot(history_dict["loss"], c="g", label="loss")
+# ax2.legend(loc="upper left")
+
+# plt.show()
+
 import json
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -13,7 +35,8 @@ ax1.plot(history_dict["val_acc"],
          c="b", label="test_accuracy")
 ax1.legend(loc="upper left")
 
-ax2.plot(history_dict["loss"], c="g", label="loss")
+ax2.plot(history_dict["loss"], c="g", label="train_loss")
+ax2.plot(history_dict["val_loss"], c="purple", label="test_loss")
 ax2.legend(loc="upper left")
 
 plt.show()
